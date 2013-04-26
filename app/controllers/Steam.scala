3045ff629c0ccffff5065ced97fa694ebb4c9d51
Étienne Vallette d'Osia <evo@zenexity.com>
2013-04-26 17:11:50 +0200

    Clean Steam's login code

--- app/controllers/Steam.scala
+++ app/controllers/Steam.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import play.api._
 import play.api.mvc.{AsyncResult, Action}
 import play.api.Logger
 import play.api.libs.openid.OpenID
@@ -8,30 +9,35 @@ import play.api.libs.json.JsString
 import play.api.libs.json.Json
 
 object Steam extends  ExtController {
-  def opendIDUrl = Action(parse.json) { implicit request =>
-    request.body.\("openid").asOpt[JsString] match {
-      case None =>  Logger.info("Steam.login bad request"); BadRequest("")
-      case Some(openid) => Async {
-        OpenID.redirectURL(openid.value, routes.Steam.openIDCallback.absoluteURL(), Seq.empty, Seq.empty, Some("http://localhost"))
-          .map{ url => Ok(Json.obj( "ok" -> true, "url" -> url )) }
-          .recover { case e: Throwable =>
-            Logger.error(e.getStackTraceString)
-            Logger.error(e.getMessage)
-            BadRequest(Json.obj( "ok" -> false ))
-          }
-      }
+  def opendIDUrl = Action { implicit request =>
+    Async {
+      OpenID.redirectURL(
+        openID = "http://steamcommunity.com/openid",
+        callbackURL = routes.Steam.openIDCallback.absoluteURL(),
+        axRequired = Seq.empty,
+        axOptional = Seq.empty,
+        realm = Some("http://localhost")
+      )
+        .map{ url =>
+          Logger.debug(s"Redirect to $url")
+          Redirect(url)
+        }
+        .recover { case e: Throwable =>
+          Logger.error("error", e)
+          BadRequest(Json.obj( "ok" -> false ))
+        }
     }
   }
 
   def openIDCallback = Action { implicit request =>
-    Logger.info(request.headers.toString())
-    Logger.info(request.body.toString)
+    Logger.info("headers: " + request.headers.toString())
+    Logger.info("body: " + request.body.toString)
+    Logger.info("querystring: " + request.queryString.toString)
     AsyncResult(
       OpenID.verifiedId
         .map( i => Ok(i.id + "\n" + i.attributes) )
         .recover { case e: Throwable =>
-          Logger.error(e.getStackTraceString)
-          Logger.error(e.getMessage)
+          Logger.error("error", e)
           Redirect(routes.Application.main(""))
         }
     )
