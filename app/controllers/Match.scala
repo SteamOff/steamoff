96682d5083b46d5aed1dc6c95a91a947feb87f9a
Arthur Gautier <aga@zenexity.com>
2013-04-26 17:16:07 +0200

    Adds a utils for pushing calendar

--- app/controllers/Match.scala
+++ app/controllers/Match.scala
@@ -7,7 +7,8 @@ import ws.GoogleCalendarWS
 object Match extends ExtController with Authentication {
 
   def make = GoogleAuthenticated { implicit user => implicit request =>
-    GoogleCalendarWS.registerEvent(request.session.get("accessToken").get)
+    GoogleCalendarWS.registerEvent(request.session.get("accessToken").get, "Left 4 Dead 2", "2013-04-26T17:00:00+02:00",
+    "2013-04-26T18:00:00+02:00", Seq("aga@zenexity.com", "evo@zenexity.com"))
     Ok("blah")
   }
 
--- app/ws/GoogleCalendarWS.scala
+++ app/ws/GoogleCalendarWS.scala
@@ -5,29 +5,86 @@ import play.api.libs.ws.WS
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
+import play.api.libs.json._
+
 object GoogleCalendarWS {
   case class AccessToken(token: String)
 
   val BASE_URL = "https://www.googleapis.com"
   val CALENDAR_LIST = "/calendar/v3/users/me/calendarList"
+  def eventInsert(calId: String) = s"/calendar/v3/calendars/${calId}/events"
 
   private def url(url: String)(implicit accessToken: AccessToken) = WS
     .url(BASE_URL + url)
     .withHeaders("Authorization" -> s"Bearer ${accessToken.token}")
     //.withQueryString("key" -> "Z2FXYku49-VwJVV4dLLMuwW6")
 
-  def registerEvent(accessToken: String): Future[Any] = {
+  def registerEvent(accessToken: String, game: String, start: String, end: String, attendees: Seq[String]): Future[Any] = {
     implicit val t = AccessToken(accessToken)
 
 
-    val resp = url(CALENDAR_LIST).get
-    resp.foreach{ r =>
-      println(r)
-      println(r.status)
-      println(r.body)
+    val req = url(CALENDAR_LIST)
+      .withQueryString("minAccessRole" -> "owner")
+    println(req.headers)
+    val resp = req.get
+    resp.flatMap{
+      case r if r.status == 200 =>
+        val j = r.json
+        val id: Option[String] = (j \ "items" \\ "id").headOption.flatMap(_.asOpt[String])
+
+        id.map{ calId =>
+          val body = Json.obj(
+            "attendees" -> JsArray(
+              attendees.map{email: String =>
+                Json.obj("email" -> email, "responseStatus" -> "needsAction")
+              }
+              ),
+            "start" -> Json.obj("dateTime" -> start),
+            "end" -> Json.obj("dateTime" -> end),
+            "summary" -> s"Let's play $game",
+            //"gadget" -> Json.obj(
+            //  //"link" -> "steam://run/8870",
+            //  "link" -> "http://localhost:9100/blah.xml",
+            //  //"display" -> "chip",
+            //  "display" -> "icon",
+            //  "iconLink" -> "http://www.playframework.com/assets/images/favicon.png",
+            //  "title" -> "play now",
+            //  "type" -> "application/x-google-gadgets+xml",
+            //  "width" -> 100,
+            //  "height" -> 100//,
+            //  //"preferences" -> Json.obj()
+            //  ),
+            "reminders" -> Json.obj(
+              "useDefault" -> false,
+              "overrides" -> Json.arr(
+                Json.obj("method" -> "email", "minutes" -> 1),
+                Json.obj("method" -> "email", "minutes" -> 2),
+                Json.obj("method" -> "email", "minutes" -> 3),
+                Json.obj("method" -> "email", "minutes" -> 4),
+                Json.obj("method" -> "email", "minutes" -> 5)
+              )
+            )
+            )
+          val req = url(eventInsert(calId))
+            .withQueryString("sendNotifications" -> "true")
+          val resp = req.post(body)
+          resp.foreach { r =>
+            println(r)
+            println(r.body)
+          }
+
+          resp
+
+          
+        }.getOrElse(Future.failed(CalendarLookupFailed))
+
+
+      case _ => Future.failed(CalendarLookupFailed)
     }
     resp
   }
 
+  case object CalendarLookupFailed extends Throwable
 }
+
 
